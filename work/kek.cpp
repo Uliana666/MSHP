6bf59df197bf5b148893d40b7fd7c6123257ffda
Uliana Parkina <uliana.parkina@gmail.com>
2024-09-22 12:02:51 +0300

    Lesson 2

--- work/kek.cpp
+++ work/kek.cpp
@@ -6,23 +6,4 @@ using namespace std;
 
 using ll = long long;
 
-int main() {
-    ll n, p;
-    cin >> n >> p;
-    vector<ll> a(n);
-    for (auto& e : a) cin >> e;
-    auto comp = [&](int a, int b) {
-        if (a % p < b % p) {
-            return true;
-        }
-        if (a % p > b % p) {
-            return false;
-        }
-        return a < b;
-    };
-
-    sort(a.begin(), a.end(), comp);
-
-    for (auto i : a) cout << i << ' ';
-    cout << endl;
-}
+int main() { cout << -10 % 3 << endl; }
